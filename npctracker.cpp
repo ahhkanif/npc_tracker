ef7b92d41cd2da2a1874429d0542f18975acf5d3
zipben <zipben@gmail.com>
2012-10-29 21:14:06 -0400

    trying it this way again

--- npctracker.cpp
+++ npctracker.cpp
@@ -16,8 +16,7 @@ struct npcstruct {
 	};
 
 int main () {
-	ifstream npccountout;
-	ofstream npccountin;
+	npcstruct npc;
 	ifstream npcfileout;
 	ofstream npcfilein;  
 	int npctotal;
@@ -29,38 +28,31 @@ int main () {
 		cout << "3)View NPC\n";
 		cout << "4)Exit\n";
 		cin >> x;
-		switch (x){
+		switch(x){
 			case 1:
-			int npccount;
-			npccountin.open ("npccount.txt");
-			npccountin << npccount;
-			npccountin.close();
-			npccountout.open ("npccount.txt");
-			npccountout << npccount;
-			npccountout.close();
-			npcstruct npc [5];
-			npc.location = npccount;
-			cout << "NPC Name:";
-			cin >> npc.name;
-			cout << "Bio:";
-			cin >> npc.bio;
-			cout << "Quirks:";
-			cin >> npc.quirk;
-			npcfilein.open ("npcinfo.txt", ios::app );
-			npcfilein << npc.name << npc.bio << npc.quirk;
-			npcfilein.close();
+				cout << "NPC Name:";
+				cin >> npc.name;
+				cout << "Bio:";
+				cin >> npc.bio;
+				cout << "Quirks:";
+				cin >> npc.quirk;
+				npcfilein.open ("npcinfo.txt", ios::app );
+				npcfilein << npc.name; 
+				npcfilein << npc.bio;
+				npcfilein << npc.quirk;
+				npcfilein.close();
 			break;
 			case 2:
-			//this is where the code for modifying an NPC would go
+				//this is where the code for modifying an NPC would go
 			break;
 			case 3:
-			//This is where the code for viewing an npc would go
+				//This is where the code for viewing an npc would go
 			break;
 			case 4:
-			offswitch = false;
+				offswitch = false;
 			break;
 			default:
-			cout << "That was not an option\n";
+				cout << "That was not an option\n";
 			break;
 			}
 	}
